b80aeae7e4b3f61adb60571f4190e466daaa9e61
Rehan Fazal <rehanfazal@live.com>
2024-06-15 23:18:25 +0200

    updated the sct.py script with pipeline method, which would ideally would help to make changes in the processing easier

--- sct/sct.py
+++ sct/sct.py
@@ -15,70 +15,108 @@ class TextCleaner:
         self.NormaliseText = normtext.NormaliseText()
         self.ProcessStopwords = stopwords.ProcessStopwords()
         self.GeneralNER = ner.GeneralNER()
+        self.pipeline = []
+        self.language = None
+        self.init_pipeline()
+    
+    def init_pipeline(self):
+        # Initialize pipeline steps based on config
+        if config.CHECK_DETECT_LANGUAGE:
+            self.pipeline.append(self.detect_language)
+        
+        if config.CHECK_FIX_BAD_UNICODE:
+            self.pipeline.append(self.fix_bad_unicode)
+        if config.CHECK_TO_ASCII_UNICODE:
+            self.pipeline.append(self.to_ascii_unicode)
+        
+        if config.CHECK_REPLACE_HTML:
+            self.pipeline.append(self.replace_html)
+        if config.CHECK_REPLACE_URLS:
+            self.pipeline.append(self.replace_urls)
+        if config.CHECK_REPLACE_EMAILS:
+            self.pipeline.append(self.replace_emails)
+        if config.CHECK_REPLACE_YEARS:
+            self.pipeline.append(self.replace_years)
+        if config.CHECK_REPLACE_PHONE_NUMBERS:
+            self.pipeline.append(self.replace_phone_numbers)
+        if config.CHECK_REPLACE_NUMBERS:
+            self.pipeline.append(self.replace_numbers)
+        if config.CHECK_REPLACE_CURRENCY_SYMBOLS:
+            self.pipeline.append(self.replace_currency_symbols)
+        
+        if config.CHECK_NER_PROCESS:
+            self.pipeline.append(self.ner_process)
+        
+        if config.CHECK_REMOVE_ISOLATED_LETTERS:
+            self.pipeline.append(self.remove_isolated_letters)
+        if config.CHECK_REMOVE_ISOLATED_SPECIAL_SYMBOLS:
+            self.pipeline.append(self.remove_isolated_special_symbols)
+        if config.CHECK_NORMALIZE_WHITESPACE:
+            self.pipeline.append(self.normalize_whitespace)
     
     def process(self, text):
         text = str(text)
-
-        language = None
-        if config.CHECK_DETECT_LANGUAGE:
-            language = str(resources.DETECTOR.detect_language_of(text)).split(".")[-1]
-
-        text = self.apply_normalization(text)
-        text = self.apply_replacements(text)
-        text = self.apply_ner_processing(text, language)
-        text = self.apply_special_processing(text)
-
-        if config.CHECK_STATISTICAL_MODEL_PROCESSING:
-            stext = self.apply_statistical_model_processing(text, language)
-            return text, stext, language
         
-        return text, language
+        for step in self.pipeline:
+            # print(f"Before {step.__name__}: {text}")  # Debug print
+            text = step(text)
+            # print(f"After {step.__name__}: {text}")  # Debug print
+            
+        if config.CHECK_STATISTICAL_MODEL_PROCESSING:
+            stext = self.statistical_model_processing(text)
+            return text, stext, self.language
+        
+        return text, self.language
 
-    def apply_normalization(self, text):
-        if config.CHECK_FIX_BAD_UNICODE:
-            text = self.NormaliseText.fix_bad_unicode(text)
-        if config.CHECK_TO_ASCII_UNICODE:
-            text = self.NormaliseText.to_ascii_unicode(text)
+    def detect_language(self, text):
+        self.language = str(resources.DETECTOR.detect_language_of(text)).split(".")[-1]
         return text
 
-    def apply_replacements(self, text):
-        if config.CHECK_REPLACE_HTML:
-            text = self.ProcessContacts.replace_html(text, replace_with=config.REPLACE_WITH_HTML)
-        if config.CHECK_REPLACE_URLS:
-            text = self.ProcessContacts.replace_urls(text, replace_with=config.REPLACE_WITH_URL)
-        if config.CHECK_REPLACE_EMAILS:
-            text = self.ProcessContacts.replace_emails(text, replace_with=config.REPLACE_WITH_EMAIL)
-        if config.CHECK_REPLACE_YEARS:
-            text = self.ProcessDateTime.replace_years(text, replace_with=config.REPLACE_WITH_YEARS)
-        if config.CHECK_REPLACE_PHONE_NUMBERS:
-            text = self.ProcessContacts.replace_phone_numbers(text, replace_with=config.REPLACE_WITH_PHONE_NUMBERS)
-        if config.CHECK_REPLACE_NUMBERS:
-            text = self.ProcessContacts.replace_numbers(text, replace_with=config.REPLACE_WITH_NUMBERS)
-        if config.CHECK_REPLACE_CURRENCY_SYMBOLS:
-            text = self.ProcessSpecialSymbols.replace_currency_symbols(text, replace_with=config.REPLACE_WITH_CURRENCY_SYMBOLS)
-        return text
+    def fix_bad_unicode(self, text):
+        return self.NormaliseText.fix_bad_unicode(text)
 
-    def apply_ner_processing(self, text, language):
-        if config.CHECK_NER_PROCESS:
-            ner_words = self.GeneralNER.ner_process(text, config.POSITIONAL_TAGS, config.NER_CONFIDENCE_THRESHOLD, language)
-            text = self.ProcessStopwords.remove_words_from_string(text, ner_words)
-        return text
+    def to_ascii_unicode(self, text):
+        return self.NormaliseText.to_ascii_unicode(text)
 
-    def apply_special_processing(self, text):
-        if config.CHECK_REMOVE_ISOLATED_LETTERS:
-            text = self.ProcessSpecialSymbols.remove_isolated_letters(text)
-        if config.CHECK_REMOVE_ISOLATED_SPECIAL_SYMBOLS:
-            text = self.ProcessSpecialSymbols.remove_isolated_special_symbols(text)
-        if config.CHECK_NORMALIZE_WHITESPACE:
-            text = self.NormaliseText.normalize_whitespace(text, no_line_breaks=True)
-        return text
+    def replace_html(self, text):
+        return self.ProcessContacts.replace_html(text, replace_with=config.REPLACE_WITH_HTML)
 
-    def apply_statistical_model_processing(self, text, language):
-        stext = text
+    def replace_urls(self, text):
+        return self.ProcessContacts.replace_urls(text, replace_with=config.REPLACE_WITH_URL)
+
+    def replace_emails(self, text):
+        return self.ProcessContacts.replace_emails(text, replace_with=config.REPLACE_WITH_EMAIL)
+
+    def replace_years(self, text):
+        return self.ProcessDateTime.replace_years(text, replace_with=config.REPLACE_WITH_YEARS)
+
+    def replace_phone_numbers(self, text):
+        return self.ProcessContacts.replace_phone_numbers(text, replace_with=config.REPLACE_WITH_PHONE_NUMBERS)
+
+    def replace_numbers(self, text):
+        return self.ProcessContacts.replace_numbers(text, replace_with=config.REPLACE_WITH_NUMBERS)
+
+    def replace_currency_symbols(self, text):
+        return self.ProcessSpecialSymbols.replace_currency_symbols(text, replace_with=config.REPLACE_WITH_CURRENCY_SYMBOLS)
+
+    def ner_process(self, text):
+        ner_words = self.GeneralNER.ner_process(text, config.POSITIONAL_TAGS, config.NER_CONFIDENCE_THRESHOLD, self.language)
+        return self.ProcessStopwords.remove_words_from_string(text, ner_words)
+
+    def remove_isolated_letters(self, text):
+        return self.ProcessSpecialSymbols.remove_isolated_letters(text)
+
+    def remove_isolated_special_symbols(self, text):
+        return self.ProcessSpecialSymbols.remove_isolated_special_symbols(text)
+
+    def normalize_whitespace(self, text):
+        return self.NormaliseText.normalize_whitespace(text, no_line_breaks=True)
+
+    def statistical_model_processing(self, text):
         if config.CHECK_CASEFOLD:
-            stext = stext.casefold()  # lowercase
+            stext = text.casefold()  # lowercase
         if config.CHECK_REMOVE_STOPWORDS:
-            stext = self.ProcessStopwords.remove_stopwords(stext, language)
+            stext = self.ProcessStopwords.remove_stopwords(stext, self.language)
         if config.CHECK_REMOVE_PUNCTUATION:
             stext = self.ProcessSpecialSymbols.remove_punctuation(stext)
         if config.CHECK_REMOVE_ISOLATED_LETTERS:
