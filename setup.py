5b6e7597930a6e50703607e5632783faddf96a72
Rehan Fazal <rehanfazal@live.com>
2024-06-16 00:54:54 +0200

    added test cases

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='SqueakyCleanText',
     version='0.1.0',
-    author='Rehan Fazal',
+    author='Your Name',
+    author_email='your.email@example.com',
     description='A comprehensive text cleaning and preprocessing pipeline.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -24,6 +25,12 @@ setup(
         'scipy==1.10.1',
         'torch',
     ],
+    extras_require={
+        'dev': [
+            'hypothesis',
+            'faker'
+        ],
+    },
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
@@ -32,7 +39,7 @@ setup(
     python_requires='>=3.7',
     entry_points={
         'console_scripts': [
-            'nltk_downloader=nltk:download'
+            'nltk_downloader=sct.scripts.download_nltk_stopwords:main'
         ],
     },
 )
