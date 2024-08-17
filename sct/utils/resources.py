cc9e4d352bb15d812849b584b171e930e53e9cdf
Rehan Fazal <rehanfazal@live.com>
2024-08-17 17:28:52 +0200

    added the language passing support

--- sct/utils/resources.py
+++ sct/utils/resources.py
@@ -5,4 +5,5 @@ from lingua import Language, LanguageDetectorBuilder
 
 #---- Detect the Language / Also add languages to support in Future
 LANGUAGES = [Language.DUTCH, Language.ENGLISH, Language.GERMAN, Language.SPANISH]
+LANGUAGE_NAME = [(language.name).lower() for language in LANGUAGES]
 DETECTOR = LanguageDetectorBuilder.from_languages(*LANGUAGES).build()
