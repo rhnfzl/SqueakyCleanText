5c95f83dbf38cc5837caf6c6af59fa1e2ff7fb0f
Rehan Fazal <rehanfazal@live.com>
2024-08-17 14:58:13 +0200

    added the German and Spanish language support in lingua

--- sct/utils/resources.py
+++ sct/utils/resources.py
@@ -4,5 +4,5 @@ warnings.filterwarnings('ignore')
 from lingua import Language, LanguageDetectorBuilder
 
 #---- Detect the Language / Also add languages to support in Future
-LANGUAGES = [Language.DUTCH, Language.ENGLISH]
+LANGUAGES = [Language.DUTCH, Language.ENGLISH, Language.GERMAN, Language.SPANISH]
 DETECTOR = LanguageDetectorBuilder.from_languages(*LANGUAGES).build()
